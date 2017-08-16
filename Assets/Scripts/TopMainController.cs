bbbe91416ea98cfcbb74ee4698b1c9b8e788589a
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-08-16 10:55:27 +0900

    Pointerコントローラのクリック位置を取得できるようになった

--- Assets/Scripts/TopMainController.cs
+++ Assets/Scripts/TopMainController.cs
@@ -6,32 +6,6 @@ using UnityEngine.EventSystems;
 
 public class TopMainController : BaseAndroidMainController {
 
-    // Update is called once per frame
-    internal override void Update()
-    {
-        base.Update();
-
-        //if (Input.touchCount > 0)
-        //{
-        //    Touch lastTouch = Input.touches[0];
-        //    if (lastTouch.phase == TouchPhase.Ended)
-        //    {
-        //        Ray ray = Camera.main.ScreenPointToRay(lastTouch.position);
-        //        RaycastHit raycastHit = new RaycastHit();
-
-        //        if (Physics.Raycast(ray, out raycastHit))
-        //        {
-        //            GameObject hitGameObject = raycastHit.collider.gameObject;
-        //            if (hitGameObject != null)
-        //            {
-        //                TextMesh textMesh = hitGameObject.GetComponent<TextMesh>();
-        //                PushNextScene(textMesh.text);
-        //            }
-        //        }
-        //    }
-        //}
-    }
-
     public void MenuSpatialAudioClicked()
     {
         mLogger.CategoryLog(LogCategoryMethodIn);
@@ -39,16 +13,11 @@ public class TopMainController : BaseAndroidMainController {
         mLogger.CategoryLog(LogCategoryMethodOut);
     }
 
-    public void MenuSpatialAudioClicked(BaseEventData eventData)
+    public void MenuTouchPadTestClicked()
     {
         mLogger.CategoryLog(LogCategoryMethodIn);
-        MenuClicked("SpatialAudio");
+        MenuClicked("TouchPad");
         mLogger.CategoryLog(LogCategoryMethodOut);
-    }
-
-    public void AddSoundBox()
-    {
-
     }
 
     private void MenuClicked(string menuAndNextSceneName)
