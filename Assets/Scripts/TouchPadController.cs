34261d31d181eee842267d1e3decff07b2e657de
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-08-17 16:49:27 +0900

    Spatial Audioが体感できるレベル

--- Assets/Scripts/TouchPadController.cs
+++ Assets/Scripts/TouchPadController.cs
@@ -10,28 +10,31 @@ using UnityEngine.EventSystems;
 public class TouchPadController : BaseAndroidMainController
 {
     public GameObject mTouchPointerPrefab;
-    private InputHelper mInput;
+    private TouchPadClickHelper mInput;
+    private UnityEngine.UI.Text mTouchedPositionText;
 
     internal override void Start()
     {
         base.Start();
-        mInput = new InputHelper(mLogger);
+        mInput = new TouchPadClickHelper(mLogger);
+        mTouchedPositionText = GameObject.Find("TouchedPositionText").GetComponent<UnityEngine.UI.Text>();
     }
 
     internal override void Update()
     {
         base.Update();
+        ManageClick();
         ManageTouch();
     }
 
-    private void ManageTouch()
+    private void ManageClick()
     {
         mLogger.CategoryLog(LogCategoryMethodIn, "click status = " + mInput.ClickStatus);
         if (mInput.ClickStatus == ClickStatus.Up)
         {
             GameObject clickedGO = mInput.ClickedGameObject;
 
-            if(clickedGO != null)
+            if (clickedGO != null)
             {
                 mLogger.CategoryLog(LogCategoryMethodTrace, "hit ray");
                 Destroy(clickedGO);
@@ -43,6 +46,22 @@ public class TouchPadController : BaseAndroidMainController
                 GameObject newInputInstance = Instantiate(mTouchPointerPrefab, clickWorldPosition, Quaternion.identity);
                 newInputInstance.SetActive(true);
             }
+        }
+        mLogger.CategoryLog(LogCategoryMethodOut);
+    }
+
+    private void ManageTouch()
+    {
+        mLogger.CategoryLog(LogCategoryMethodIn, "touch status = " + mInput.TouchStatus);
+        if (mInput.TouchStatus == TouchStatus.Down)
+        {
+            mLogger.CategoryLog(LogCategoryMethodTrace, "touched position down(" + mInput.TouchPositionOnTouchPad.x + ", " + mInput.TouchPositionOnTouchPad.y + ")");
+            mTouchedPositionText.text = mInput.TouchPositionOnTouchPad.ToString();
+        }
+        else if (mInput.TouchStatus == TouchStatus.Up)
+        {
+            mLogger.CategoryLog(LogCategoryMethodTrace, "touched position up(" + mInput.TouchPositionOnTouchPad.x + ", " + mInput.TouchPositionOnTouchPad.y + ")");
+            mTouchedPositionText.text = mInput.TouchPositionOnTouchPad.ToString();
         }
         mLogger.CategoryLog(LogCategoryMethodOut);
     }
